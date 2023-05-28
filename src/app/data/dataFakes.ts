6c774797e500a24478b6d7ee3d89476c9faeee25
Kleber <kleber_ricardobr@yahoo.com.br>
2023-05-28 20:48:08 -0300

    Added faked data to teste routes

---
+++ src/app/data/dataFakes.ts
@@ -0,0 +1,17 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photoCover": "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/fccc6398-0f8f-11ed-aa6e-9587410378a2/tony-stark-snap.jpg",
+        "title": "MINHA NOTÍCIA",
+        "description": "Hello World!"
+    },
+
+    {
+        "id": "2",
+        "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyQ0QdiTnGxTZsg80c1y24amxSHv2LKPHtuA&usqp=CAU",
+        "title": "O INÍCIO",
+        "description": "A história do estúdios Marvel"
+    }
+]
+
+
